12e2bfde0d39717d48bc974a06154b116850be10
Rich Bubbico <rjb2182@stern.nyu.edu>
2019-07-01 22:59:55 -0400

    initial setup

---
+++ selfie_smash.py
@@ -0,0 +1,19 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0.0)
+white = (255,255,255)
+red = (255,0,0)
+
+obstacle_color = (43,58,77)
+
+selfie_width = 73
+
+
+
